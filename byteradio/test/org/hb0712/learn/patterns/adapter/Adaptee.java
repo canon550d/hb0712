5c2975d49b1f381552a24b359fa68dff1e3dae2a
shieh <shieh@greatwall>
2013-03-13 21:01:42 +0800

    添加适配器模式

---
+++ byteradio/test/org/hb0712/learn/patterns/adapter/Adaptee.java
@@ -0,0 +1,10 @@
+package org.hb0712.learn.patterns.adapter;
+
+/*
+ * Ô´½ÇÉ«
+ */
+public class Adaptee {
+	public void specificRequest(){
+		System.out.println("µçÑ¹200V");
+	}
+}
---
+++ byteradio/test/org/hb0712/learn/patterns/adapter/Target.java
@@ -0,0 +1,8 @@
+package org.hb0712.learn.patterns.adapter;
+
+/*
+ * Ä¿±ê½ÇÉ«
+ */
+public interface Target {
+	public void request();
+}
