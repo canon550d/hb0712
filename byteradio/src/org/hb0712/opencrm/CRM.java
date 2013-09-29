9f3d84816d1dbd2fe89d3a27c1d674ce3d90195f
shieh <shieh@greatwall>
2013-09-29 22:36:58 +0800

    添加一个OpenCRM项目

---
+++ byteradio/src/org/hb0712/opencrm/CRM.java
@@ -0,0 +1,15 @@
+package org.hb0712.opencrm;
+
+import org.apache.log4j.Logger;
+
+public class CRM {
+	private static Logger logger = Logger.getLogger(CRM.class);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("CRMÏîÄ¿");
+	}
+
+}
